db1ab5c84a8efece5508e8a1bd174fb687ae924e
Paula Martins <paulastsmartins@gmail.com>
2022-03-31 14:57:10 -0300

    spring

--- 0x01/spring_boot_1/com/exemplo/demo/MessageResource.java
+++ 0x01/spring_boot_1/com/exemplo/demo/MessageResource.java
@@ -9,7 +9,7 @@ public class MessageResource {
 
 	@GetMapping("/simpleMessageWelcome")
 	public String simpleMessageWelcome() {
-		return "BEM VINDO A AULA DE MICROSSERVIÃ‡O USANDO SPRING BOOT !!!";
+		return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
 	}
 	
 	@GetMapping(value = "/login")
@@ -19,10 +19,10 @@ public class MessageResource {
 		String senha = "123";
 				
 		if(usuario.isEmpty() || senha.isEmpty()) {
-			message = "USUÃ�RIO E SENHA NÃƒO INFORMADOS";
+			message = "USUÁRIO E SENHA NÃO INFORMADOS";
 		}
 		else if (usuario.length() > 15 || senha.length() >15) {
-			message = "USUÃ�RIO E SENHA INVÃ�LIDOS";
+			message = "USUÁRIO E SENHA INVÁLIDOS";
 		}else {
 			message = "LOGIN EFETUADO COM SUCESSO";
 		}
