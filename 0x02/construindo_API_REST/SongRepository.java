7d5db212e82e02c96ddc10e5e9bc257df1093254
Paula Martins <paulastsmartins@gmail.com>
2022-04-01 14:24:29 -0300

    crud

---
+++ 0x02/construindo_API_REST/SongRepository.java
@@ -0,0 +1,45 @@
+package com.paula.song.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.paula.song.model.Song;
+
+@Repository
+public class SongRepository {
+
+	private List<Song> list = new ArrayList<Song>();
+
+    public List<Song> getAllSongs() {
+     return list;
+    }
+
+    public Song getSongById(Integer id) {
+    	for(Song lista : list ) {
+	   		if(lista.getId().equals(id)) {
+	   			return lista;
+	   		}
+	   	}	   	
+	   	return null;
+      }
+    
+    public void addSong(Song s) {
+    	list.add(s);
+    }
+
+    public void updateSong(Song s) {
+    	 for (Song song : list) {
+            	if(song.getId() == s.getId()) {
+            		int i = list.indexOf(song);
+            		list.set(i, s);
+            	}
+    		}
+    }
+
+    public void removeSong(Song s) {
+       list.remove(s);
+    }
+	
+}
